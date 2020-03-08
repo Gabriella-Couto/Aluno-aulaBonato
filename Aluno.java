9c8c4fc6b1f69af68645b55c084535b9689f11b8
Gabriella Couto <gabrielasouzadocouto@gmail.com>
2020-03-08 15:42:30 -0300

    Adicionando construtor

--- Aluno.java
+++ Aluno.java
@@ -1,6 +1,15 @@
 public class Aluno{
    private String nome;
    private int id;
+
+   public Aluno(){
+
+   }
+
+   public Aluno (String nome, int id){
+      this.nome = nome;
+      this.id = id;
+   }
    
    public String getNome(){
       return nome;
